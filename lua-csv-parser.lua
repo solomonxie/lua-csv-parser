7f6e1458201773246aa95e420f340dba0b47ae63
Solomon Xie <solomonxiewise@gmail.com>
2022-02-07 19:41:20 +0800

    updated on branch [master]

--- lua-csv-parser.lua
+++ lua-csv-parser.lua
@@ -4,14 +4,16 @@ function M.parse_csv_line(line, sep)
     if not line then return nil end
     if not sep then sep = ',' end
     local values = {}
+    -- FIND THE COLUMN VALUE'S END-POSITION
     local pos = string.find(line, sep)
     if string.sub(line, 1, 1) == '[' then
+        -- DTYPE: ARRAY
         local arr_end_pos = string.find(line, ']')
         if arr_end_pos then pos = arr_end_pos + 1 end
     end
     if not pos then pos = string.len(line) + 1 end
-    local v = string.sub(line, 1, pos - 1)
-    v = string.gsub(string.gsub(v, '^%s+', ''), '%s+$', '')
+    local v = string.sub(line, 1, pos - 1)  -- GET COLUMN VALUE
+    v = string.gsub(string.gsub(v, '^%s+', ''), '%s+$', '')  -- TRIM SPACES
     if string.sub(v, 1, 1) == '"' and string.sub(v, #v, #v) == '"' then
         v = string.sub(v, 2, #v - 1)
     end
@@ -65,9 +67,11 @@ function M.parse_file_by_line(path, sep)
     if not sep then sep = ',' end
     local i = 0
     local iter = io.lines(path)
-    local headers = M.parse_csv_line(iter())
+    local header_line = iter()
+    local headers = M.parse_csv_line(header_line)
     return function ()
-        i = i +1
+        i = i + 1
+        if i == 1 then return i, header_line, headers end
         local line = iter()
         local vlist = M.parse_csv_line(line) or {}
         if vlist and #vlist > 0 then
@@ -75,7 +79,7 @@ function M.parse_file_by_line(path, sep)
             for j, col in pairs(headers) do
                 d[col] = vlist[j]
             end
-            return i, d
+            return i, line, d
         else
             print('DONE: LOADED CSV CONTENT...')
         end
